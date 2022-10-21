c25fe3af439291fa5b1de4f0fd35af0cafbac020
Cherry <109952722+cherryxcute@users.noreply.github.com>
2022-10-21 13:56:52 +0530

    Update runtime.js

--- plugins/special/runtime.js
+++ plugins/special/runtime.js
@@ -7,7 +7,7 @@ exports.run = {
    }) => {
       let _uptime = process.uptime() * 1000
       let uptime = Func.toTime(_uptime)
-      client.reply(m.chat, Func.texted('bold', `Berjalan selama :  ${uptime} `), m)
+      client.reply(m.chat, Func.texted('bold', `Total up time of bot...🍓 :  ${uptime} `), m)
    },
    error: false
 }
